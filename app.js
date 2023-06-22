bb6d75d8820b6ef79ee709dc1224d1de1329ba8a
Dhruv <prashardhruv2229@gmail.com>
2023-06-23 01:28:59 +0530

    Orders functionalities and documentation

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app= express();
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+
+
 
 //Database
 const connectDB= require('./db/connect');
@@ -17,11 +24,22 @@ const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
 const productRouter = require('./routes/productRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
-
+const orderRouter = require('./routes/orderRoutes');
 //middleware
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler')
+
+
+app.set('trust proxyy',1);
+app.use(rateLimiter({
+    windowMs: 15*60*1000,
+    max:60,
+}));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(morgan('tiny'))
 app.use(express.json());
@@ -45,6 +63,7 @@ app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products',productRouter);
 app.use('/api/v1/reviews',reviewRouter);
+app.use('/api/v1/orders',orderRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
